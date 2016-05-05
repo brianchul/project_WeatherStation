2c84e6548e947e19bbc6e62d70ff9feba74252bb
brianchul <404411711@gms.tku.edu.tw>
2016-05-05 10:25:54 +0800

    bug fix & add time adjust

--- pi_two_way.py
+++ pi_two_way.py
@@ -4,21 +4,26 @@ import serial
 import time
 
 key = '50K4ERTOTXTA8GX4'
-ser = serial.Serial('dev/ttyUSB0', 9600, timeout = 1)
-ser2 = serial.Serial('dev/ttyUSB1', 9600, timeout = 1)
+ser = serial.Serial('/dev/ttyUSB0', 9600, timeout=1)
+ser2 = serial.Serial('/dev/ttyUSB1', 9600, timeout=1)
+now = datetime.datetime.now()
+
+
 def sendData():
     try:
-        ser.write("Start_nano\r\n")
-        print "Start_nano"
+        ser.write('Start_nano\r\n')
+        print 'Start_nano'
         print ser.readline()
         tmp = int(ser.readline())
         hum = int(ser.readline())
         light = int(ser.readline())
         rain = int(ser.readline())
         airdust = int(ser.readline())
+    except:
+        print 'ERROR serial1'
     try:
-        ser.write("Start_UNO\r\n")
-        print "Start_nano"
+        ser.write('Start_UNO\r\n')
+        print 'Start_nano'
         print ser.readline()
         ser2.write(tmp)
         ser2.write(hum)
@@ -27,12 +32,24 @@ def sendData():
         ser2.write(airdust)
         UNOtmp = ser2.readline()
         UNOhum = ser2.readline()
-        
-    params = urllib.urlencode({'field1':tmp,'field2':hum,'field3':light,'field4':rain,'field5':airdust,'field6':UNOtmp,'field7':UNOhum,key':key})
-    headers = {"Content-typZZe": "application/x-www-form-urlencoded","Accept": "text/plain"}
-    conn = httplib.HTTPConnection("api.thingspeak.com:80")
+    except:
+        print 'ERROR serial2'
+
+    params = urllib.urlencode({
+        'field1': tmp,
+        'field2': hum,
+        'field3': light,
+        'field4': rain,
+        'field5': airdust,
+        'field6': UNOtmp,now
+        'field7': UNOhum,
+        'key': key,
+        })
+    headers = {'Content-typZZe': 'application/x-www-form-urlencoded',
+               'Accept': 'text/plain'}
+    conn = httplib.HTTPConnection('api.thingspeak.com:80')
     try:
-        conn.request("POST", "/update", params, headers)
+        conn.request('POST', '/update', params, headers)
         response = conn.getresponse()
         print response.status
         print response.reason
@@ -42,8 +59,25 @@ def sendData():
         print "connection failed"
 
 
-        
-if __name__ == "__main__":
+def sendTime():
+    try:
+        ser2.write('Adj_time')
+        print 'Adj_time'
+        ser2.write(time.strftime("%S"))
+        ser2.write(time.strftime("%M"))
+        ser2.write(time.strftime("%H"))
+        ser2.write(time.strftime("%w"))
+        ser2.write(time.strftime("%d"))
+        ser2.write(time.strftime("%m"))
+        ser2.write(time.strftime("%Y"))
+    except:
+
+        # seconds, minutes, hours, day of the week, day of the month, month, year
+
+        print "ERROR adjTime"
+
+
+if __name__ == '__main__':
     while True:
         sendData()
         time.sleep(15)
